359bae75cb739c70131d0ab033c63d23bc668bf8
lastikun-nu <nunoxu641580806@gmail.com>
2026-09-23 13:06:18 +0200

    Rellenar datos de conexion

--- ClicArte/backend/conexion.php
+++ ClicArte/backend/conexion.php
@@ -1,1 +1,12 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = ""; 
+$dbname = "ClicArte";
 
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Conexión fallida: " . $conn->connect_error);
+}
+?>
